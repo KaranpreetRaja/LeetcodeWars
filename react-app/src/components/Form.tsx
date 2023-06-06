a57e84721811b77865aedcf25838974f1ba0d33d
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-06-05 20:30:22 -0400

    Update Form

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -4,15 +4,18 @@ type Props = {};
 
 const Form = (props: Props) => {
   const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
+  const maxPlayersRef = useRef<HTMLInputElement>(null);
 
-  const [person, setPerson] = useState({ name: "", age: 0 });
+  const [room, setRoom] = useState({ name: "", maxPlayers: "" });
 
   const handelSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current != null && ageRef.current != null) {
+    if (nameRef.current != null && maxPlayersRef.current != null) {
       console.log(
-        "name" + nameRef.current.value + " age" + ageRef.current.value
+        "name" +
+          nameRef.current.value +
+          " maxPlayers" +
+          maxPlayersRef.current.value
       );
     }
   };
@@ -25,19 +28,24 @@ const Form = (props: Props) => {
         </label>
         <input
           ref={nameRef}
-          onChange={(event) =>
-            setPerson({ ...person, name: event.target.value })
-          }
+          onChange={(event) => setRoom({ ...room, name: event.target.value })}
           id="name"
           type="text"
           className="form-control"
+          value={room.name}
         />
       </div>
       <div className="mb-3">
-        <label htmlFor="age" className="form">
+        <label htmlFor="maxPlayers" className="form">
           Max Players
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          ref={maxPlayersRef}
+          id="maxPlayers"
+          type="number"
+          className="form-control"
+          value={room.maxPlayers}
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Create Room
